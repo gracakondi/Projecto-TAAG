0dfe3076d1b171833606ac78364fdb17f8df1151
mr0xff <worktrabalho633@gmail.com>
2025-02-04 23:26:39 +0100

    Adicionado Cancelamento

--- main.c
+++ main.c
@@ -7,15 +7,16 @@
 #include <stdint.h>
 
 #define INICIAL 10 // nº de Threads/"clientes"
+#define TRUE 1
 
 typedef struct {
-    int pnr;
-    int reserva;
-    int consulta;
-    int cancelamento;
+	int pnr;
+	int reserva;
+	int consulta;
+	int cancelamento;
 } buffer;
 
-buffer *buff;  // Mudança para alocação dinâmica
+buffer *regicao_critica;  // Mudança para alocação dinâmica
 
 pthread_mutex_t mutex;
 sem_t sem_reserva;
@@ -23,119 +24,144 @@ sem_t sem_consulta;
 
 void* reserva(void* args);
 void* consulta(void* args);
+void* cancelamento(void* args);
+
 void ver_dados();
-void* Thread(void* args);
+void* Thread(void* args); // thread principal
 void tratamento_interrupcao();
 
 int main() {
-    pthread_t threads[INICIAL];
-    int i;
+	pthread_t threads[INICIAL];
+	int i;
 
-    srand(time(NULL));
+	srand(time(NULL));
 
-    // Alocação dinâmica de memória para o buffer
-    buff = (buffer*)malloc(INICIAL * sizeof(buffer));
-    if (buff == NULL) {
-        printf("Erro ao alocar memória para o buffer.\n");
-        return 1;
-    }
+	// Alocação dinâmica de memória para o buffer
+	regicao_critica = (buffer*)malloc(INICIAL * sizeof(buffer));
 
-    pthread_mutex_init(&mutex, NULL);
-    sem_init(&sem_reserva, 0, 5);  // Limite de 5 threads de reserva ao mesmo tempo
-    sem_init(&sem_consulta, 0, 5); // Limite de 5 threads de consulta ao mesmo tempo
+	if (regicao_critica == NULL) {
+		printf("Erro ao alocar memória para o buffer.\n");
+		return 1;
+	}
 
-    for (i = 0; i < 10; i++) {
-        intptr_t buffer_index = i;
-        pthread_create(&threads[i], NULL, Thread, (void*)buffer_index);
-    }
+	pthread_mutex_init(&mutex, NULL);
+	sem_init(&sem_reserva, 0, 5);  // Limite de 5 threads de reserva ao mesmo tempo
+	sem_init(&sem_consulta, 0, 5); // Limite de 5 threads de consulta ao mesmo tempo
 
-    for (i = 0; i < INICIAL; i++) {
-        pthread_join(threads[i], NULL);
-    }
+	for (i = 0; i < 10; i++) {
+		intptr_t buffer_index = i;
+		pthread_create(&threads[i], NULL, Thread, (void*)buffer_index);
+	}
 
-    // Liberação da memória alocada dinamicamente
-    free(buff);
-    pthread_mutex_destroy(&mutex);
-    sem_destroy(&sem_reserva);
-    sem_destroy(&sem_consulta);
+	for (i = 0; i < INICIAL; i++)
+		pthread_join(threads[i], NULL);
 
-    printf("Finalizado.\n");
-    return 0;
+	// Liberação da memória alocada dinamicamente
+	free(regicao_critica);
+	pthread_mutex_destroy(&mutex);
+	sem_destroy(&sem_reserva);
+	sem_destroy(&sem_consulta);
+
+	printf("Finalizado.\n");
+	return 0;
 }
 
 void* reserva(void* args) {
-    sem_wait(&sem_reserva);
-    tratamento_interrupcao();  // Simula interrupção durante a operação
+	sem_wait(&sem_reserva);
+	tratamento_interrupcao();  // Simula interrupção durante a operação
 
-    int buf_index = (intptr_t)args;
-    int pnr = (unsigned int)pthread_self();
+	int buf_index = (intptr_t)args;
+	int pnr = (unsigned int)pthread_self();
 
-    pthread_mutex_lock(&mutex);
-    buff[buf_index].pnr = pnr;
-    buff[buf_index].reserva = 1;
-    buff[buf_index].consulta = 0;
-    buff[buf_index].cancelamento = 0;
-    pthread_mutex_unlock(&mutex);
+	pthread_mutex_lock(&mutex);
+	regicao_critica[buf_index].pnr = pnr;
+	regicao_critica[buf_index].reserva = 1;
+	regicao_critica[buf_index].consulta = 0;
+	regicao_critica[buf_index].cancelamento = 0;
+	pthread_mutex_unlock(&mutex);
 
-    printf("[Reserva] PNR da thread: %d\n", pnr);
-    ver_dados();
+	printf("[Reserva] PNR da thread: %d\n", pnr);
+	ver_dados();
 
-    sem_post(&sem_reserva);
-    return NULL;
+	sem_post(&sem_reserva);
+	return NULL;
 }
 
 void* consulta(void* args) {
-    sem_wait(&sem_consulta);
-    tratamento_interrupcao();  // Simula interrupção durante a operação
+	sem_wait(&sem_consulta);
+	tratamento_interrupcao();  // Simula interrupção durante a operação
 
-    int buf_index = (intptr_t)args;
-    int pnr = (unsigned int)pthread_self();
+	int buf_index = (intptr_t)args;
+	int pnr = (unsigned int)pthread_self();
 
-    pthread_mutex_lock(&mutex);
-    buff[buf_index].pnr = pnr;
-    buff[buf_index].reserva = 0;
-    buff[buf_index].consulta = 1;
-    buff[buf_index].cancelamento = 0;
-    pthread_mutex_unlock(&mutex);
+	pthread_mutex_lock(&mutex);
+	regicao_critica[buf_index].pnr = pnr;
+	regicao_critica[buf_index].reserva = 0;
+	regicao_critica[buf_index].consulta = 1;
+	regicao_critica[buf_index].cancelamento = 0;
+	pthread_mutex_unlock(&mutex);
 
-    printf("[Consulta] PNR da thread: %d\n", pnr);
-    ver_dados();
+	printf("[Consulta] PNR da thread: %d\n", pnr);
+	ver_dados();
 
-    sem_post(&sem_consulta);
-    return NULL;
+	sem_post(&sem_consulta);
+	return NULL;
+}
+
+void* cancelamento(void* args) {
+	sem_wait(&sem_consulta);
+	tratamento_interrupcao();  // Simula interrupção durante a operação
+
+	int buf_index = (intptr_t)args;
+	int pnr = (unsigned int)pthread_self();
+
+	pthread_mutex_lock(&mutex);
+	regicao_critica[buf_index].pnr = pnr;
+	regicao_critica[buf_index].reserva = 0;
+	regicao_critica[buf_index].consulta = 0;
+	regicao_critica[buf_index].cancelamento = 1;
+	pthread_mutex_unlock(&mutex);
+
+	printf("[Cancelamento] PNR da thread: %d\n", pnr);
+	ver_dados();
+
+	sem_post(&sem_consulta);
+	return NULL;
 }
 
 void ver_dados() {
-    int i; 
-    pthread_mutex_lock(&mutex);
-    printf("\n=== DADOS ATUAIS ===\n");
-    for (i = 0; i < INICIAL; i++) {
-        printf("PNR: %d | Reserva: %d | Consulta: %d | Cancelamento: %d\n",
-               buff[i].pnr, buff[i].reserva, buff[i].consulta, buff[i].cancelamento);
-    }
-    pthread_mutex_unlock(&mutex);
+	int i; 
+	pthread_mutex_lock(&mutex);
+	printf("\n=== Processos/Threads em execução ===\n");
+
+	for (i = 0; i < INICIAL; i++)
+		printf("PNR: %d | Reserva: %d | Consulta: %d | Cancelamento: %d\n", regicao_critica[i].pnr, regicao_critica[i].reserva, regicao_critica[i].consulta, regicao_critica[i].cancelamento);
+	
+	pthread_mutex_unlock(&mutex);
 }
 
 void* Thread(void* args) {
-    int index = (intptr_t)args;
-    int i; 
-    for (i = 0; i < 5; i++) {
-        int f = rand() % 2;
-        pthread_t thread;
+	int index = (intptr_t)args;
+	int i; 
+	
+	while (TRUE) {
+		int f = rand() % 3;
+		pthread_t thread;
 
-        if (f == 0) {
-            pthread_create(&thread, NULL, reserva, (void*)(intptr_t)index);
-        } else {
-            pthread_create(&thread, NULL, consulta, (void*)(intptr_t)index);
-        }
+		if (f == 0)
+			pthread_create(&thread, NULL, reserva, (void*)(intptr_t)index);
+		else if (f == 1)
+			pthread_create(&thread, NULL, consulta, (void*)(intptr_t)index);
+		else
+			pthread_create(&thread, NULL, cancelamento, (void*)(intptr_t)index);
 
-        pthread_join(thread, NULL);
-    }
+		pthread_join(thread, NULL);
+	}
 
-    return NULL;
+	return NULL;
 }
 
 // Função para simular a interrupção durante o processo
 void tratamento_interrupcao() {
-    sleep(1);  // Simula uma "interrupção" do processo, com uma pausa de 1 segundo
+  sleep(1);  // Simula uma "interrupção" do processo, com uma pausa de 1 segundo
 }
